c9f7dc2562a8e80d52e10ad45f2aae5647733ebe
Abhilash Chaurasiya <abhilash.vc888@gmail.com>
2025-02-05 00:42:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,17 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [state,setState] = useState("")
+  const handel = (e)=>{
+    let input = e.target.value
+    setState("Hello"+input)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+    <label>Enter your name</label>
+    <input type="text" onPress={handel}/>
+    <h2>{state}</h2>
     </div>
   )
 }
